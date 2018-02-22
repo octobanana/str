7213f29bb34d12ef5c9ff10301f21b2c297f7d68
Brett Robinson <octo.banana93@gmail.com>
2018-02-22 01:38:47 -0700

    refactor tests into individual functions

--- main.c
+++ main.c
@@ -5,186 +5,286 @@
 #include <stdlib.h>
 #include <assert.h>
 
-void str_print(str_t *str);
+void print_title(const char *title);
+void print_str(str_t *str);
 
-void str_print(str_t *str)
+void test_new();
+void test_append();
+void test_clear();
+void test_reserve();
+void test_set();
+void test_insert();
+void test_resize();
+void test_compare();
+void test_move();
+void test_erase();
+void test_escape();
+
+void print_title(const char *title)
+{
+  printf("\n----------------\n");
+  printf("%s", title);
+  printf("\n----------------\n");
+}
+
+void print_str(str_t *str)
 {
   printf("str: %s\n", str->str);
   printf("len: %d\n", str->len);
   printf("cap: %d\n", str->cap);
 }
 
-int main()
+void test_new()
 {
-  // new
-  printf("new\n");
+  print_title("new");
+
   str_t str;
   str_new(&str);
-  str_print(&str);
+
+  print_str(&str);
+
   assert(strcmp(str.str, "") == 0);
   assert(str.len == 0);
   assert(str.cap == 1);
 
-  // append
-  printf("\nappend\n");
-  str_cappend(&str, "hello", 5);
-  str_print(&str);
-  assert(strcmp(str.str, "hello") == 0);
+  str_delete(&str);
+
+  str_t *strp = malloc(sizeof(str_t));
+  str_new(strp);
+
+  print_str(strp);
+
+  assert(strcmp(strp->str, "") == 0);
+  assert(strp->len == 0);
+  assert(strp->cap == 1);
+
+  str_delete(strp);
+  free(strp);
+}
+
+void test_append()
+{
+  print_title("append");
+
+  str_t *str = malloc(sizeof(str_t));
+  str_new(str);
+
+  str_t str2;
+  str_new(&str2);
+
+  str_cappend(str, "Hello, World!", 8);
+  print_str(str);
+
+  assert(strcmp(str->str, "Hello, W") == 0);
+  assert(str->len == 8);
+  assert(str->cap == 9);
+
+  str_cset(&str2, "orld!", 5);
+
+  str_append(str, &str2);
+
+  print_str(str);
+  assert(strcmp(str->str, "Hello, World!") == 0);
+
+  str_delete(str);
+  free(str);
+  str_delete(&str2);
+}
+
+void test_clear()
+{
+  print_title("clear");
+
+  str_t str;
+  str_new(&str);
+
+  str_cappend(&str, "Hello, World!", 13);
+  print_str(&str);
+  str_clear(&str);
+  print_str(&str);
+
+  assert(strcmp(str.str, "") == 0);
+  assert(str.len == 0);
+  assert(str.cap == 14);
+
+  str_delete(&str);
+}
+
+void test_reserve()
+{
+  print_title("reserve");
+
+  str_t str;
+  str_new(&str);
+
+  str_reserve(&str, 64);
+  print_str(&str);
+
+  assert(strcmp(str.str, "") == 0);
+  assert(str.len == 0);
+  assert(str.cap == 65);
+
+  str_delete(&str);
+}
+
+void test_set()
+{
+  print_title("set");
+
+  str_t str;
+  str_new(&str);
+
+  str_cappend(&str, "test", 4);
+  str_cset(&str, "Hello, World!", 13);
+  print_str(&str);
+
+  assert(strcmp(str.str, "Hello, World!") == 0);
+  assert(str.len == 13);
+  assert(str.cap == 14);
+
+  str_delete(&str);
+}
+
+void test_insert()
+{
+  print_title("insert");
+
+  str_t *str = malloc(sizeof(str_t));
+  str_new(str);
+
+  str_cappend(str, "Hello, World!", 13);
+  assert(str_insert(str, "Great Big ", 7, 10) == 0);
+  print_str(str);
+
+  assert(strcmp(str->str, "Hello, Great Big World!") == 0);
+  assert(str->len == 23);
+  assert(str->cap == 24);
+
+  str_delete(str);
+  free(str);
+}
+
+void test_resize()
+{
+  print_title("resize");
+
+  str_t str;
+  str_new(&str);
+
+  str_cappend(&str, "Hello, World!", 13);
+  print_str(&str);
+  str_resize(&str, 5);
+  print_str(&str);
+
+  assert(strcmp(str.str, "Hello") == 0);
   assert(str.len == 5);
   assert(str.cap == 6);
 
-  // clear
-  printf("\nclear\n");
-  str_clear(&str);
-  str_print(&str);
-  assert(strcmp(str.str, "") == 0);
-  assert(str.len == 0);
-  assert(str.cap == 6);
-
-  // set
-  printf("\nset\n");
-  str_cset(&str, "alligator", 9);
-  str_print(&str);
-  assert(strcmp(str.str, "alligator") == 0);
-  assert(str.len == 9);
-  assert(str.cap == 10);
-
-  // insert
-  printf("\ninsert\n");
-  str_insert(&str, 3, "hi", 2);
-  str_print(&str);
-  assert(strcmp(str.str, "allhiigator") == 0);
-  assert(str.len == 11);
-  assert(str.cap == 12);
-
-  str_insert(&str, 0, "hello ", 6);
-  str_print(&str);
-  assert(strcmp(str.str, "hello allhiigator") == 0);
-  assert(str.len == 17);
-  assert(str.cap == 18);
-
-  str_insert(&str, str.len, " world", 6);
-  str_print(&str);
-  assert(strcmp(str.str, "hello allhiigator world") == 0);
-  assert(str.len == 23);
-  assert(str.cap == 24);
-
-  // resize
-  printf("\nresize\n");
-  str_resize(&str, 3);
-  str_print(&str);
-  assert(strcmp(str.str, "hel") == 0);
-  assert(str.len == 3);
-  assert(str.cap == 4);
-
-  str_resize(&str, 11);
-  str_print(&str);
-  assert(strcmp(str.str, "hel") == 0);
-  assert(str.len == 3);
-  assert(str.cap == 12);
-
-  str_cappend(&str, "lo world", 7);
-  str_print(&str);
-  assert(strcmp(str.str, "hello worl") == 0);
-  assert(str.len == 10);
-  assert(str.cap == 12);
   str_delete(&str);
+}
 
-  // compare
-  printf("\ncompare\n");
-  int eq = 0;
-  char bl[6];
+void test_compare()
+{
+  print_title("compare");
+
   str_t str1;
   str_new(&str1);
+
+  str_cappend(&str1, "Hello, World!", 13);
+  print_str(&str1);
+
   str_t str2;
   str_new(&str2);
-  str_cset(&str1, "hello", 5);
-  str_cset(&str2, "hello", 5);
-  assert(strcmp(str1.str, "hello") == 0);
-  assert(strcmp(str2.str, "hello") == 0);
 
-  eq = str_compare(&str1, &str2);
-  printf("str1: %s\n", str1.str);
-  printf("str2: %s\n", str2.str);
-  if (eq == 0)
-  {
-    sprintf(bl, "true");
-  }
-  else
-  {
-    sprintf(bl, "false");
-  }
-  printf("eq: %s\n", bl);
-  assert(strcmp(str1.str, str2.str) == 0);
+  str_cappend(&str2, "Hello, World!", 13);
+  print_str(&str2);
 
-  str_cset(&str2, "hi", 2);
-  eq = str_compare(&str1, &str2);
-  printf("str1: %s\n", str1.str);
-  printf("str2: %s\n", str2.str);
-  if (eq == 0)
-  {
-    sprintf(bl, "true");
-  }
-  else
-  {
-    sprintf(bl, "false");
-  }
-  printf("eq: %s\n", bl);
-  assert(strcmp(str1.str, str2.str) != 0);
+  assert(str_compare(&str1, &str2) == 0);
+  assert(str_ccompare(&str1, "Hello, World!", 13) == 0);
 
-  str_cset(&str2, "hello world", 11);
-  eq = str_compare(&str1, &str2);
-  printf("str1: %s\n", str1.str);
-  printf("str2: %s\n", str2.str);
-  if (eq == 0)
-  {
-    sprintf(bl, "true");
-  }
-  else
-  {
-    sprintf(bl, "false");
-  }
-  printf("eq: %s\n", bl);
-  assert(strcmp(str1.str, str2.str) != 0);
+  str_insert(&str1, "Great Big ", 7, 10);
+  print_str(&str1);
+  print_str(&str2);
+  assert(str_compare(&str1, &str2) != 0);
+  assert(str_ccompare(&str1, "Hello, World!", 13) != 0);
 
-  // delete
-  printf("\ndelete\n");
   str_delete(&str1);
   str_delete(&str2);
+}
 
-  // move
-  printf("\nmove\n");
-  str_t m1;
-  str_new(&m1);
-  str_t m2;
-  str_new(&m2);
-  str_cset(&m1, "hello", 5);
-  str_cset(&m2, "world", 5);
-  str_move(&m1, &m2);
-  str_print(&m1);
-  assert(strcmp(m1.str, "world") == 0);
-  str_delete(&m1);
+void test_move()
+{
+  print_title("move");
 
-  // escape
-  printf("\nescape\n");
-  str_t str_e;
-  str_new(&str_e);
-  str_cset(&str_e, "{\"key\":\"value\n\"}", 16);
-  str_escape(&str_e);
-  str_print(&str_e);
-  assert(strcmp(str_e.str, "{\\\"key\\\":\\\"value\\n\\\"}") == 0);
-  str_delete(&str_e);
+  str_t str1;
+  str_new(&str1);
 
-  // erase
-  printf("\nerase\n");
-  str_t str3;
-  str_new(&str3);
-  str_cappend(&str3, "hello", 5);
-  str_erase(&str3, 2, 2);
-  str_print(&str3);
-  assert(strcmp(str3.str, "heo") == 0);
-  assert(str3.len == 3);
-  assert(str3.cap == 6);
+  str_cappend(&str1, "Hello, World!", 13);
+  print_str(&str1);
+
+  str_t str2;
+  str_new(&str2);
+
+  str_cappend(&str2, "Tiny Elephants", 14);
+  print_str(&str2);
+
+  str_move(&str1, &str2);
+  print_str(&str1);
+  assert(strcmp(str1.str, "Tiny Elephants") == 0);
+
+  str_delete(&str2);
+}
+
+void test_erase()
+{
+  print_title("erase");
+
+  str_t str;
+  str_new(&str);
+
+  str_cappend(&str, "Hello, Great Big World!", 23);
+  print_str(&str);
+  str_erase(&str, 6, 10);
+  print_str(&str);
+
+  assert(strcmp(str.str, "Hello, World!") == 0);
+  assert(str.len == 13);
+  assert(str.cap == 24);
+
+  str_delete(&str);
+}
+
+void test_escape()
+{
+  print_title("escape");
+
+  str_t str;
+  str_new(&str);
+
+  char json[] = "{\"key\":\"value\n\"}";
+  str_cappend(&str, json, strlen(json));
+  print_str(&str);
+  str_escape(&str);
+  print_str(&str);
+
+  assert(strcmp(str.str, "{\\\"key\\\":\\\"value\\n\\\"}") == 0);
+
+  str_delete(&str);
+}
+
+int main()
+{
+  test_new();
+  test_append();
+  test_clear();
+  test_reserve();
+  test_set();
+  test_insert();
+  test_resize();
+  test_compare();
+  test_move();
+  test_erase();
+  test_escape();
 
   return 0;
 }
